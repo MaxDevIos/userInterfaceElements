2cfa8eaabdfc29cf0a3c751d93275dc589ac931b
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-07-15 18:44:37 +0300

    Date Picker

--- userInterfaceElements/ViewController.swift
+++ userInterfaceElements/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var segmentedControl: UISegmentedControl!
-    @IBOutlet var showLabel: UILabel!
-    @IBOutlet var slider: UISlider!
+    @IBOutlet private var segmentedControl: UISegmentedControl!
+    @IBOutlet private var showLabel: UILabel!
+    @IBOutlet private var slider: UISlider!
+    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var button: UIButton!
+    @IBOutlet var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +37,14 @@ class ViewController: UIViewController {
         slider.thumbTintColor = .blue
         
         showLabel.text = String(slider.value)
+        
+        textField.placeholder = "Enter your name"
+        
+        button.layer.cornerRadius = 20
     
     }
     
-    @IBAction func SegmentedControlButtonPressed() {
+    @IBAction private func SegmentedControlButtonPressed() {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
             showLabel.text = "The first segment is selected"
@@ -53,12 +60,49 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func sliderAction() {
+    @IBAction private func sliderAction() {
         showLabel.text = String(slider.value)
         
         let sliderValue = CGFloat(slider.value)
         
         view.backgroundColor = view.backgroundColor?.withAlphaComponent(sliderValue)
     }
+    
+    @IBAction private func buttonAction() {
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            showAlert(with: "Text field is empty", and: "Please, enter your name")
+            return
+        }
+        
+        if let _ = Double(inputText) {
+            showAlert(with: "Wrong format", and: "Please, enter your name")
+            return
+        }
+        
+        showLabel.text = inputText
+    }
+    
+    @IBAction func datePickerAction() {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ru-RU")
+        
+        showLabel.text = dateFormatter.string(from: datePicker.date)
+    }
 }
+
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.textField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        
+    }
+}
+
 
